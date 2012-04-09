adf0a2f0bf62dd1f06f63db65009eb0b2a7bac91
HumanProxy <benoit.molenda@gmail.com>
2012-04-09 11:21:34 +0200

    bundle gem

---
+++ lib/devise_oauth2_providable_mongoid.rb
@@ -0,0 +1,5 @@
+require "devise_oauth2_providable_mongoid/version"
+
+module DeviseOauth2ProvidableMongoid
+  # Your code goes here...
+end
---
+++ lib/devise_oauth2_providable_mongoid/version.rb
@@ -0,0 +1,3 @@
+module DeviseOauth2ProvidableMongoid
+  VERSION = "0.0.1"
+end
